d6e6aea2c85d305afaac1e2a46a77d9e181f9aa2
mtennekes <mtennekes@gmail.com>
2025-02-26 09:47:44 +0100

    added crs_basemap

--- R/onload.R
+++ R/onload.R
@@ -2,8 +2,7 @@
 	requireNamespace("tmap", quietly = TRUE)
 	requireNamespace("deckgl", quietly = TRUE)
 	requireNamespace("data.table", quietly = TRUE)
-	tmap::tmapMode("deck", "DeckGL", pitch = 60, basemap.show = TRUE)
-	
+	tmap::tmapMode("deck", "DeckGL", pitch = 60, basemap.show = TRUE, crs_basemap = 4326)
 }
 
 .TMAP_DECKGL = new.env(FALSE, parent = globalenv())
